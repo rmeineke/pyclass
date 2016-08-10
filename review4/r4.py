168d61051ea8f9f6586d15221974d87f6eec6f53
Robert Meineke <rmeineke@gmail.com>
2016-08-10 03:45:16 -0700

    review 4 complete

---
+++ review4/r4.py
@@ -0,0 +1,11 @@
+name_dict = {'one': 'robert', 'two': 'jane', 'three':'alfred', 'four': 'brian', 3: 'question1'}
+
+print name_dict
+
+#for name in sorted(name_dict): 
+    #print name.capitalize(), ":", name_dict[name]
+
+print name_dict
+print
+print
+print name_dict[3]
