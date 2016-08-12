99bfc7fcaa747437e908398cf730e90c620904f4
Robert Meineke <rmeineke@gmail.com>
2016-08-12 05:17:35 -0700

    more assignment 3 work

--- assignment3/application/driver/hw11_3.py
+++ assignment3/application/driver/hw11_3.py
@@ -5,6 +5,26 @@ sys.path.insert(0, os.path.abspath('../utils'))
 
 import hw11_2
 
-print(hw11_2.Palindromize('ab-c-ba'))
+def check_directory_for_palindromes(directory):
+    print('Entering check_directory_for_palindromes')
+    print(directory)
+    
+    #os.path.walk ... deprecated to os.walk in python3
+    os.walk(directory, hw11_2.Palindromize, 'Checking: ')
+    
+    
+def main():
+    check_this_directory = '../../application'
+    for directory, subdir_list, file_list in os.walk(check_this_directory, topdown=True):
+        print('Checking directory: {}'.format(directory))
+        for filename in file_list:
+            print('\t{}'.format(filename))
+            print('\t\topen file')
+            print('\t\tgrab words from file')
+            print(hw11_2.Palindromize('ahhhhha'))
 
-print(hw11_2.Palindromize('ammmmmba'))
+    os.walk(directory, hw11_2.Palindromize2, 'Checking: ')
+
+
+if __name__ == '__main__':
+    main()
--- assignment3/application/utils/hw11_2.py
+++ assignment3/application/utils/hw11_2.py
@@ -19,3 +19,24 @@ def Palindromize(phrase):
         return reversed_string
     
     
+def Palindromize2(phrase):
+    print('## Palindromize2 ##')
+    # make sure everything passed in is seen as a string object
+    # the numbers (12321, etc.) caused a crash without this
+    phrase = str(phrase)
+    
+    #too short
+    if len(phrase) < 4:
+        return
+    
+    #lowercase, strip spaces/punctuation, and reverse it
+    string = phrase.lower()
+    string = ''.join(ch for ch in string if ch.isalnum())
+    reversed_string = string[::-1]
+    
+    if string != reversed_string:
+        return None
+    else:
+        return reversed_string
+    
+    
