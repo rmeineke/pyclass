1c707654df61fa26a5254bde2a0d5f1b51dceccf
Robert Meineke <rmeineke@gmail.com>
2016-08-12 03:12:24 -0700

    preliminary work on HW3 project

---
+++ assignment3/application/driver/hw11_3.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../utils'))
+
+import hw11_2
+
+print(hw11_2.Palindromize('ab-c-ba'))
+
+print(hw11_2.Palindromize('ammmmmba'))
---
+++ assignment3/application/utils/hw11_2.py
@@ -0,0 +1,21 @@
+def Palindromize(phrase):
+    
+    # make sure everything passed in is seen as a string object
+    # the numbers (12321, etc.) caused a crash without this
+    phrase = str(phrase)
+    
+    #too short
+    if len(phrase) < 4:
+        return
+    
+    #lowercase, strip spaces/punctuation, and reverse it
+    string = phrase.lower()
+    string = ''.join(ch for ch in string if ch.isalnum())
+    reversed_string = string[::-1]
+    
+    if string != reversed_string:
+        return None
+    else:
+        return reversed_string
+    
+    
