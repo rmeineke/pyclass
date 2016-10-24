8e9c3845e3febe3a4027193310780aadd99ac42e
Robert Meineke <rmeineke@gmail.com>
2016-10-24 05:34:32 -0700

    final cleanup of 5a prior to submission

--- assignment5a/program.py
+++ assignment5a/program.py
@@ -1,15 +1,29 @@
 import logging
+import sys
 import re
 import os
 import json
+import account
 
-
+# set up for logging
+LEVELS = {'debug':logging.DEBUG,
+          'info':logging.INFO,
+          'warning':logging.WARNING,
+          'error':logging.ERROR,
+          'critical':logging.CRITICAL,
+         }
+if len(sys.argv) > 1:
+    level_name = sys.argv[1]
+    level = LEVELS.get(level_name, logging.NOTSET)
+    logging.basicConfig(level=level)
+    
+    
 def PromptForTransactionDate():
     '''
     ask the user for the date of this particular transaction
     '''
     while True:
-        date = input("Date of transaction (mm/dd/yyyy): ")
+        date = input("\nDate of transaction (mm/dd/yyyy): ")
         
         # make sure the date is proper format
         match = re.match('\d\d/\d\d/\d\d\d\d', date)
@@ -22,7 +36,7 @@ def PromptForTransactionDate():
 def PromptForTransactionDescription():
     '''
     need to have the specific type/description of
-    the transaction
+    the transaction ... append each line to a list
     '''
     descr = []
     while True:
@@ -35,10 +49,11 @@ def PromptForTransactionDescription():
 def PromptForTransactionAmount():
     '''
     looking for a dollar amount here
+    dollars and cents ... e.g., 202.13
     '''
     amt = 0
     while True:
-        amt = input('Enter amount of transaction: $')
+        amt = input('\nEnter amount of transaction: $')
         
         #here is a brain-dead regex that works for the moment
         match = re.match('\d*\.\d\d', amt)
@@ -54,13 +69,13 @@ def PromptForTransactionAmount():
         
         if match:
             return amt    
+        else:
+            print('Bad amount format ... use ###.## ... for dollars and cents')
             
             
-
 def CalculateQuarterShare(amt):
     '''
-    25%
-    one quarter of the entire bill
+    calculate one quarter of the entire fee
     '''
     amt = int(amt * .25)
     return round(amt)
@@ -68,6 +83,8 @@ def CalculateQuarterShare(amt):
     
 def CalculateMasterAccountShare(qtr_share, amt):
     '''
+    separate calculation for the 'master' account 
+    
     this needs to be calculated slightly differently
     as there may be a penny or two slop over based 
     upon money being divided four ways and then rounded
@@ -76,16 +93,15 @@ def CalculateMasterAccountShare(qtr_share, amt):
     return amt - non_master_ttl
     
     
-    
 def GetDataFilePath():
     '''
-    look in the "Data" directory for the accounts file.
+    look in the "/Data" directory for the accounts file.
     return the full pathname ... or bomb out if not 
     found
     '''
-    data_file = os.path.join('.', 'Data', 'accounts.json' )
+    data_file = os.path.join('.', 'Data', 'accounts.json')
     if not os.path.exists(data_file):
-        print("accounts file {} not found . . . exiting".format(data_file))
+        logger.critical('accounts file -{}- not found ... exiting'.format(data_file))
         exit()
     return data_file
     
@@ -94,27 +110,31 @@ def main():
     logger = logging.getLogger()
     logger.debug('Entering Main')
     
-    DEBUG = 1
+    DEBUG = 0
     if not DEBUG:
         date = PromptForTransactionDate()
-        logger.debug('date: %s', date)
+        logger.debug('date: {}'.format(date))
         descr = PromptForTransactionDescription()
-        logger.debug('descr: %s', descr)
+        logger.debug('descr: {}'.format(descr))
         amount = PromptForTransactionAmount()
-        logger.debug('amount: %s', amount)
+        logger.debug('amount: {}'.format(amount))
     else:
+        logger.debug('using test/debug input')
         date = '10/15/2016'
-        descr = 'This is a test'
+        descr = ['This test input', 'is a ', 'FOUR Line example', 'Testing ... dummy text']
         amount = 401.00
 
+
+    # collect information from the user
     transaction_amount = int(float(amount) * 100)
-    logger.debug('transaction_amount: %s', transaction_amount)
+    logger.debug('transaction_amount: {}'.format(transaction_amount))
     
     qtr_share = CalculateQuarterShare(transaction_amount)
-    logger.debug('qtr_share: %s', qtr_share)
+    logger.debug('qtr_share: {}'.format(qtr_share))
     
     master_account_share = CalculateMasterAccountShare(qtr_share, transaction_amount)
-    logger.debug('master_account_share: %s', master_account_share)
+    logger.debug('master_account_share: {}'.format(master_account_share))
+    
     
     # now that the basic information has been gathered we'll
     # read the json file that contains the individual account
@@ -125,60 +145,96 @@ def main():
     with open(data_file) as data_file:
         data = json.load(data_file)
 
-    # instantiate the account objects and 
-    # append them to a list of accounts
+    # instantiate the account objects
+    # then immediately call for the object to update itself
     accts = []
     for acct in data["accounts"]:
-        acct_data_file = os.path.join('.', 'Data', str(acct['address']) + '.well.txt')
-        logger.debug(acct_data_file)
-        
-        
-        # print('data_file ... {}'.format(data_file))
-        
-        # open each file .... find the last balance in the account
-        # should look like this ... 
-        # 
-        # 07/21/2016  -----  Jul 2016 Usage, Electric      $  14.28   $  65.77
-        #
-        # dollar sign
-        # any number of characters
-        # decimal point
-        # 2 decimal characters (the 'cents')
-        # any number of spaces
-        # dollar sign
-        # any number of characters
-        # decimal point
-        # 2 decimal characters (the 'cents')
-        # the end of the string
-        bal_regex = re.compile(r'\$.*.\d\d\s+\$(.*.\d\d)$') 
-        last_bal = 0
-        with open(acct_data_file) as acct_file:
-            for line in acct_file:
-                line = line.strip()
-                mo = bal_regex.search(line)
-                if mo:
-                    # be sure to strip any spaces from the match
-                    last_balance = mo.group(1).strip()
-            print('{} /// {}'.format(last_balance, line))
-
-        last_balance = int(float(last_balance) * 100)
-        current_bal = 0
-        if acct['master'] == 'yes':
-            current_bal = int(float(last_balance) + master_share)
-            amount_to_record = master_share
-            print(' {} ... {} ... {}'.format(last_balance, master_share, current_bal))
+        acct_obj = account.Account(acct['address'], acct['name'], acct['reads_in'], acct['master'], os.path.join('.', 'Data', acct['file']))
+        if acct_obj.master == 'no':
+            acct_obj.WriteAdminFeeToAccountFile(date, qtr_share, descr)
         else:
-            current_bal = int(float(last_balance) + qtr_share)
-            amount_to_record = qtr_share
-            print('last bal: {} ... qtr_share: {} ... current_bal: {}'.format(last_balance, qtr_share, current_bal))
-        current_bal = int(current_bal * 100)
-        print()
-        
-        # write out the information to each account file
-        # print(acct)
-        
-        
-
+            acct_obj.WriteAdminFeeToAccountFile(date, master_account_share, descr)
+ 
+ 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     main()
+
+
+'''
+////// with test/debug data
+
+[05:21] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py debug
+DEBUG:root:Entering Main
+DEBUG:root:using test/debug input
+DEBUG:root:transaction_amount: 40100
+DEBUG:root:qtr_share: 10025
+DEBUG:root:master_account_share: 10025
+[05:21] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py critical
+[05:21] robertm -- /home/robertm/programming/pyclass/assignment5a $ 
+
+
+
+
+////// interactive sessions
+
+[05:22] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py debug
+DEBUG:root:Entering Main
+
+Date of transaction (mm/dd/yyyy): dkfj
+Bad date format ... use mm/dd/yyyy
+
+Date of transaction (mm/dd/yyyy): djjdjdjdjd;
+Bad date format ... use mm/dd/yyyy
+
+Date of transaction (mm/dd/yyyy): 10/23/2016
+DEBUG:root:date: 10/23/2016
+Enter description ["q" to quit]: This is a test
+Enter description ["q" to quit]: entry.
+Enter description ["q" to quit]: q
+DEBUG:root:descr: ['This is a test', 'entry.']
+DEBUG:root:<class 'list'>
+
+Enter amount of transaction: $kdfj
+Bad amount format ... use ###.## ... for dollars and cents
+
+Enter amount of transaction: $lsls
+Bad amount format ... use ###.## ... for dollars and cents
+
+Enter amount of transaction: $199.99
+DEBUG:root:amount: 199.99
+DEBUG:root:transaction_amount: 19999
+DEBUG:root:qtr_share: 4999
+DEBUG:root:master_account_share: 5002
+[05:23] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py critical
+
+Date of transaction (mm/dd/yyyy): dkfj
+Bad date format ... use mm/dd/yyyy
+
+Date of transaction (mm/dd/yyyy): 10/24/2016
+Enter description ["q" to quit]: Yearly well fee 
+Enter description ["q" to quit]: To: Monterey County Health Dept.
+Enter description ["q" to quit]: Paid by electr. funds xfer
+Enter description ["q" to quit]: q
+
+Enter amount of transaction: $dksfj
+Bad amount format ... use ###.## ... for dollars and cents
+
+Enter amount of transaction: $jdjd
+Bad amount format ... use ###.## ... for dollars and cents
+
+Enter amount of transaction: $199.99
+[05:24] robertm -- /home/robertm/programming/pyclass/assignment5a $ 
+[05:24] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py critical
+
+Date of transaction (mm/dd/yyyy): 10/25/2016
+Enter description ["q" to quit]: Administrative test
+Enter description ["q" to quit]: Three lines should be
+Enter description ["q" to quit]: in the output
+Enter description ["q" to quit]: q
+
+Enter amount of transaction: $199.01
+[05:26] robertm -- /home/robertm/programming/pyclass/assignment5a $ 
+
+
+'''
