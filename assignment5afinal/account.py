0a33e6085205e74d6ec016781c5d9e3f223b86e0
Robert Meineke <rmeineke@gmail.com>
2016-10-26 19:52:38 -0700

    assignment 5a final files

---
+++ assignment5afinal/account.py
@@ -0,0 +1,102 @@
+'''
+This account.py module houses the Account class 
+and any account-related functionality
+'''
+
+import re
+
+class Account:
+    '''
+    The Account class is populated once the main
+    program is run. All data is retrieved from the 
+    master account list which is in a JSON file in 
+    the /Data directory that should sit under the
+    current working directory
+    '''
+    
+    def __init__(self, address, name, reads_in, master, datafile):
+        self.address = address
+        self.name = name
+        self.readsin = reads_in
+        self.master = master
+        self.datafile = datafile
+        
+        # this will 'self-fire' a function
+        # that retrieves and initializes the current 
+        # account balance for this particular account object
+        self.SetCurrentBalance()
+    
+
+    def __str__(self):
+        return "name: %s\naddr: %s\nreads in: %s\nmaster?: %s\ncurrent_balance:%s" \
+                % (self.name, self.address, self.readsin, self.master, self.current_balance)
+    
+    
+    def SetCurrentBalance(self):
+        '''
+        Parse each account data file and retrieve the 
+        account's current balance ... then set the 
+        object's balance property
+        
+        This function is self-fired when the object is first
+        initialized ... see the __init__
+        '''
+        
+        # open each file .... find the last balance in the account
+        # should look something like this ... 
+        # 
+        # 07/21/2016  -----  Jul 2016 Usage, Electric      $  14.28   $  65.77
+
+
+        # I always document my regexes as shown below ... 
+        # because I can never figure them out later without
+        # some kind of 'roadmap'
+        
+        # regex notes:
+        # ============
+        # dollar sign
+        # any number of characters
+        # decimal point
+        # 2 decimal characters (the 'cents')
+        # any number of spaces
+        # dollar sign
+        # any number of characters
+        # decimal point
+        # 2 decimal characters (the 'cents')
+        # the end of the string
+        
+        bal_regex = re.compile(r'\$.*.\d\d\s+\$(.*.\d\d)$') 
+        last_bal = 0
+        with open(self.datafile) as acct_file:
+            for line in acct_file:
+                line = line.strip()
+                mo = bal_regex.search(line)
+                if mo:
+                    # be sure to strip any spaces from the match
+                    last_balance = mo.group(1).strip()
+
+        self.current_balance = int(float(last_balance) * 100)
+        
+          
+    def WriteAdminFeeToAccountFile(self, date, amount, transaction_details):
+        '''
+        Append the fee information to the account object's data file
+        '''
+        
+        # open file and append the transaction to the end
+        with open(self.datafile, 'a') as acct_file:
+            # first line of transaction detail
+            acct_file.write('\n{}  -----  {:30}${:7.2f}   ${:7.2f}\n'. \
+                        format(date, transaction_details[0], (amount/100), \
+                        ((self.current_balance + amount)/100)))
+            
+            # remaining lines of the transaction detail            
+            for line in transaction_details[1:]:
+                    acct_file.write('                   {}\n'.format(line))
+            
+        
+        # legacy perl code ... for example output spacing
+        """
+        printf {$OUT} "%s  -----  $mo $yr Usage, Electric      \$%7.2f   \$%7.2f\n",
+        $self->get_date_last_read(), $self->get_bill(), $self->get_bal();
+        """
---
+++ assignment5afinal/program.py
@@ -0,0 +1,229 @@
+import logging
+import sys
+import re
+import os
+import json
+import account
+from dateutil.parser import parse
+    
+def IsDate(str):
+    try: 
+        parse(str)
+        return True
+    except ValueError:
+        return False
+    
+def PromptForTransactionDate():
+    '''
+    ask the user for the date of this particular transaction
+    '''
+    while True:
+        date = input("\nDate of transaction (mm/dd/yyyy): ")
+        if IsDate(date):
+            return date
+        print("Bad date format ... use mm/dd/yyyy");
+
+
+def PromptForTransactionDescription():
+    '''
+    need to have the specific type/description of
+    the transaction ... append each line to a list
+    '''
+    descr = []
+    while True:
+        resp = input('Enter description ["q" to quit]: ')
+        if resp == 'q':
+            return descr
+        descr.append(resp)
+    
+
+def PromptForTransactionAmount():
+    '''
+    looking for a dollar amount here
+    dollars and cents ... e.g., 202.13
+    '''
+    while True:
+        amt = input('\nEnter amount of transaction: $')
+        try:
+            return str(float(amt.replace('$','')))
+        except ValueError:
+            print('Bad amount format ... use ###.## ... for dollars and cents')
+            
+            
+def CalculateQuarterShare(amt):
+    '''
+    calculate one quarter of the entire fee
+    '''
+    return round(amt * .25)
+    
+    
+def CalculateMasterAccountShare(qtr_share, amt):
+    '''
+    separate calculation for the 'master' account 
+    
+    this needs to be calculated slightly differently
+    as there may be a penny or two slop over based 
+    upon money being divided four ways and then rounded
+    '''
+    non_master_ttl = 3 * qtr_share
+    return amt - non_master_ttl
+    
+    
+def GetDataFilePath():
+    '''
+    look in the "/Data" directory for the accounts file.
+    return the full pathname ... or bomb out if not 
+    found
+    '''
+    data_file = os.path.join('.', 'Data', 'accounts.json')
+    if not os.path.exists(data_file):
+        logger.critical('accounts file -{}- not found ... exiting'.format(data_file))
+        exit()
+    return data_file
+    
+
+def main():
+    
+    # set up for logging
+    LEVELS = {'debug':logging.DEBUG,
+              'info':logging.INFO,
+              'warning':logging.WARNING,
+              'error':logging.ERROR,
+              'critical':logging.CRITICAL,
+             }
+    if len(sys.argv) > 1:
+        level_name = sys.argv[1]
+        level = LEVELS.get(level_name, logging.NOTSET)
+        logging.basicConfig(level=level)
+        
+    logger = logging.getLogger()
+    logger.debug('Entering Main')
+    
+    DEBUG = 0
+    if not DEBUG:
+        date = PromptForTransactionDate()
+        logger.debug('date: {}'.format(date))
+        descr = PromptForTransactionDescription()
+        logger.debug('descr: {}'.format(descr))
+        amount = PromptForTransactionAmount()
+        logger.debug('amount: {}'.format(amount))
+    else:
+        logger.debug('using test/debug input')
+        date = '10/15/2016'
+        descr = ['This test input', 'is a ', 'FOUR Line example', 'Testing ... dummy text']
+        amount = 401.00
+
+
+    # collect information from the user
+    transaction_amount = int(float(amount) * 100)
+    logger.debug('transaction_amount: {}'.format(transaction_amount))
+    
+    qtr_share = CalculateQuarterShare(transaction_amount)
+    logger.debug('qtr_share: {}'.format(qtr_share))
+    
+    master_account_share = CalculateMasterAccountShare(qtr_share, transaction_amount)
+    logger.debug('master_account_share: {}'.format(master_account_share))
+    
+    
+    # now that the basic information has been gathered we'll
+    # read the json file that contains the individual account
+    # information ...
+    
+    data_file = GetDataFilePath()
+    # open the file and read it in as json data
+    with open(data_file) as data_file:
+        data = json.load(data_file)
+
+    # instantiate the account objects
+    # then immediately call for the object to update itself
+    accts = []
+    for acct in data["accounts"]:
+        acct_obj = account.Account(acct['address'], acct['name'], acct['reads_in'], acct['master'], os.path.join('.', 'Data', acct['file']))
+        if acct_obj.master == 'no':
+            acct_obj.WriteAdminFeeToAccountFile(date, qtr_share, descr)
+        else:
+            acct_obj.WriteAdminFeeToAccountFile(date, master_account_share, descr)
+ 
+ 
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    main()
+
+
+'''
+////// with test/debug data
+
+[05:21] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py debug
+DEBUG:root:Entering Main
+DEBUG:root:using test/debug input
+DEBUG:root:transaction_amount: 40100
+DEBUG:root:qtr_share: 10025
+DEBUG:root:master_account_share: 10025
+[05:21] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py critical
+[05:21] robertm -- /home/robertm/programming/pyclass/assignment5a $ 
+
+
+
+
+////// interactive sessions
+
+[05:22] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py debug
+DEBUG:root:Entering Main
+
+Date of transaction (mm/dd/yyyy): dkfj
+Bad date format ... use mm/dd/yyyy
+
+Date of transaction (mm/dd/yyyy): djjdjdjdjd;
+Bad date format ... use mm/dd/yyyy
+
+Date of transaction (mm/dd/yyyy): 10/23/2016
+DEBUG:root:date: 10/23/2016
+Enter description ["q" to quit]: This is a test
+Enter description ["q" to quit]: entry.
+Enter description ["q" to quit]: q
+DEBUG:root:descr: ['This is a test', 'entry.']
+DEBUG:root:<class 'list'>
+
+Enter amount of transaction: $kdfj
+Bad amount format ... use ###.## ... for dollars and cents
+
+Enter amount of transaction: $lsls
+Bad amount format ... use ###.## ... for dollars and cents
+
+Enter amount of transaction: $199.99
+DEBUG:root:amount: 199.99
+DEBUG:root:transaction_amount: 19999
+DEBUG:root:qtr_share: 4999
+DEBUG:root:master_account_share: 5002
+[05:23] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py critical
+
+Date of transaction (mm/dd/yyyy): dkfj
+Bad date format ... use mm/dd/yyyy
+
+Date of transaction (mm/dd/yyyy): 10/24/2016
+Enter description ["q" to quit]: Yearly well fee 
+Enter description ["q" to quit]: To: Monterey County Health Dept.
+Enter description ["q" to quit]: Paid by electr. funds xfer
+Enter description ["q" to quit]: q
+
+Enter amount of transaction: $dksfj
+Bad amount format ... use ###.## ... for dollars and cents
+
+Enter amount of transaction: $jdjd
+Bad amount format ... use ###.## ... for dollars and cents
+
+Enter amount of transaction: $199.99
+[05:24] robertm -- /home/robertm/programming/pyclass/assignment5a $ 
+[05:24] robertm -- /home/robertm/programming/pyclass/assignment5a $ python3 program.py critical
+
+Date of transaction (mm/dd/yyyy): 10/25/2016
+Enter description ["q" to quit]: Administrative test
+Enter description ["q" to quit]: Three lines should be
+Enter description ["q" to quit]: in the output
+Enter description ["q" to quit]: q
+
+Enter amount of transaction: $199.01
+[05:26] robertm -- /home/robertm/programming/pyclass/assignment5a $ 
+
+
+'''
